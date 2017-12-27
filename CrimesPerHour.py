5069cfac787c2e32d4d465f7963f47e6d5e5dda8
Ayliana Teitelbaum <ateitel2@mail.yu.edu>
2017-12-27 14:22:09 -0500

    Functional Version

--- CrimesPerHour.py
+++ CrimesPerHour.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python
 import re
-def CrimesPerHour():
+def crimesPerHour():
     input_file = "ComplaintData.txt"
     violentCrimeRegex = "Robbery|Harrassment|Assault|Rape|Arson|Murder|Kidnapping"
     violentCrimeRegex = re.compile(violentCrimeRegex.upper())
     f = open(input_file)
     day = ''
     hour = ''
-    crimeCount = 0
+    crimeCount = {}
+    
+
     for line in f:
+        #field0 = date and time, field1 = crime 
         fields = line. split('|')
         fields[1] = fields[1][:2]
+
+        #if theres a new day/hour combo
         if not (fields[0] == day and fields[1] == hour):
-            yield day, hour, crimeCount
+            if day:
+                yield day +' '+  hour, crimeCount
+
+            #reset hourly crime count    
             day = fields[0]
             hour = fields[1]
-            crimeCount = 0
-        if re.search(violentCrimeRegex, fields[2]):
-            crimeCount+=1
-    yield day, hour, crimeCount
+            crimeCount = {}
+            
+        #update crime count
+        violentCrime = re.search(violentCrimeRegex, fields[2])
+        if violentCrime:
+            crime = violentCrime.group(0).lower()
+            if crime in crimeCount:
+                crimeCount[crime]+=1
+            else:
+                crimeCount[crime] = 1
+    yield day + ' '+ hour, crimeCount
+
+if __name__ == "__main__":
+    x = list(crimesPerHour())
+    for i in x:
+        print(i)
 
 
         
